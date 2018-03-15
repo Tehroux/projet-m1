46ad3c3741c28c8c797561fcfeca21b728ee0e98
HANNEDOUCHE Alexandre <hannedouche.alex@gmail.com>
2018-03-15 15:59:14 +0100

    add remove user

--- mainLoop/mainLoop.js
+++ mainLoop/mainLoop.js
@@ -1,5 +1,6 @@
 const createUser = require('../user/user.js').createUser;
+const removeUser = require('../user/user.js').removeUser;
 
 exports.mainLoop = (db) => {
-	createUser(db, 'test');
+	removeUser(db, 'test');
 };
--- user/user.js
+++ user/user.js
@@ -1,4 +1,11 @@
+const collectionName = 'cloudUsers';
+
 exports.createUser = (db, userName) => {
-	 const userCollection = db.collection('cloudUsers');
-	userCollection.insertMany([{userName: userName}]);
+	const userCollection = db.collection(collectionName);
+	userCollection.insertOne({ userName: userName });
+};
+
+exports.removeUser = (db, userName) => {
+	const userCollection = db.collection(collectionName);
+	userCollection.deleteOne({userName: userName });
 };

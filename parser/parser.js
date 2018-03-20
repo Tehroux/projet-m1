6225a34cb1edf6abf31366fa645ed80be5224e8b
HANNEDOUCHE Alexandre <hannedouche.alex@gmail.com>
2018-03-20 10:32:25 +0100

    deleted node_module from online repo
    node_modules comntaine like 70000 line of code.
    give a bad appreciation of actual work done.

--- parser/parser.js
+++ parser/parser.js
@@ -1,6 +1,11 @@
+const SEP = /:/
 const AUTH_REQ = /^USER_ADD/;
 
 exports.parser = (request) => {
 	if( request.match(AUTH_REQ))
 		console.log('> user auth');
 };
+
+function auth(request) {
+	 
+}

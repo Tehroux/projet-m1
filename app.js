9cbbe3c60e7e82c92e141ddd64585b7a5fe86fe1
HANNEDOUCHE Alexandre <hannedouche.alex@gmail.com>
2018-03-29 17:43:03 +0200

    moving thing arround

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const net = require('net');
+const mongoose = require('mongoose');
 
-const parser = require('../parser/parser.js').parser;
+const User = require('./model/user.js'); 
 
+const URL = 'mongodb://locahost:27017/cloud';
+
+// server creation
 const server = net.createServer((client) => {
 	console.log('> client connected');
 	client.on('end', () => {
 		console.log('> client disconnected');
 	});
 	client.on('data', (data) => {
-		parser(data.toString());
+		console.log('> data: ' + data.toString())
 	});
-	client.write('hello\r\n');
+	client.write('MOTD: your are connected');
 	client.pipe(client);
 });
 
@@ -21,4 +25,5 @@ server.on('error', (err) => {
 server.listen(8123, () => {
 	console.log('> server bound');
 });
+
 
